8ab4fc81d57de676401537e0e99833fa257df8fd
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-17 17:50:28 +0100

    activate port

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const cors = require("cors");
 const express = require("express");
 
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT;
 app.use(express.json());
 
 app.use(cors());
-
-const app = express();
-app.get();
-app.get();
-app.get();
-app.get();
-app.post();
-app.post();
-app.put();
-app.put();
-app.delete();
-app.delete();
+// app.get();
+// app.get();
+// app.get();
+// app.get();
+// app.post();
+// app.post();
+// app.put();
+// app.put();
+// app.delete();
+// app.delete();
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
