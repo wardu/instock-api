b6661f86e24dcc5211803c1a575b733631f91d97
Pauline Jeremie <pauline.jeremie@gmail.com>
2022-10-18 16:36:01 +0100

    Start building delete function

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 const warehouseData = require("../data/warehouses.json");
 const fs = require("fs");
 
-router.get("/warehouses", (req, res) => {
+router.get("/", (req, res) => {
   console.log("/warehouses is working");
   res.status(200).json("/warehouses is active");
 });
+
+router.delete("/:id", (req, res) => {
+  console.log("Delete warehouse");
+  res.status(200).json("Delete warehouse is working");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const cors = require("cors");
 const express = require("express");
-
 const app = express();
+const router = require("express").Router();
+const warehouseRoutes = require("./routes/warehouses");
+
 require("dotenv").config();
 
 const port = process.env.PORT;
 app.use(express.json());
 
 app.use(cors());
-// app.get();
-// app.get();
-// app.get();
-// app.get();
-// app.post();
-// app.post();
-// app.put();
-// app.put();
-// app.delete();
-// app.delete();
+
+app.use("/warehouses", warehouseRoutes);
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
